ca5936a2f811c178465159ad2daf389db715d89e
Kapi4000 <89608670+Kapi4000@users.noreply.github.com>
2021-12-26 13:49:04 -0300

    Update Song.hx

--- source/Song.hx
+++ source/Song.hx
@@ -5,30 +5,21 @@ import haxe.Json;
 import haxe.format.JsonParser;
 import lime.utils.Assets;
 
-#if sys
-import sys.io.File;
-import sys.FileSystem;
-#end
-
 using StringTools;
 
 typedef SwagSong =
 {
 	var song:String;
 	var notes:Array<SwagSection>;
-	var events:Array<Dynamic>;
 	var bpm:Float;
 	var needsVoices:Bool;
 	var speed:Float;
 
 	var player1:String;
 	var player2:String;
-	var player3:String; //deprecated, now replaced by gfVersion
 	var gfVersion:String;
+	var noteStyle:String;
 	var stage:String;
-
-	var arrowSkin:String;
-	var splashSkin:String;
 	var validScore:Bool;
 }
 
@@ -36,50 +27,15 @@ class Song
 {
 	public var song:String;
 	public var notes:Array<SwagSection>;
-	public var events:Array<Dynamic>;
 	public var bpm:Float;
 	public var needsVoices:Bool = true;
-	public var arrowSkin:String;
-	public var splashSkin:String;
 	public var speed:Float = 1;
-	public var stage:String;
 
 	public var player1:String = 'bf';
 	public var player2:String = 'dad';
-	public var player3:String = 'gf'; //deprecated
-	public var gfVersion:String = 'gf';
-
-	private static function onLoadJson(songJson) // Convert old charts to newest format
-	{
-		if(songJson.gfVersion == null)
-		{
-			songJson.gfVersion = songJson.player3;
-			songJson.player3 = null;
-		}
-
-		if(songJson.events == null)
-		{
-			songJson.events = [];
-			for (secNum in 0...songJson.notes.length)
-			{
-				var sec:SwagSection = songJson.notes[secNum];
-
-				var i:Int = 0;
-				var len:Int = sec.sectionNotes.length;
-				while(i < len)
-				{
-					var note:Array<Dynamic> = sec.sectionNotes[i];
-					if(note[1] < 0)
-					{
-						songJson.events.push([note[0], [[note[2], note[3], note[4]]]]);
-						sec.sectionNotes.remove(note);
-						len = sec.sectionNotes.length;
-					}
-					else i++;
-				}
-			}
-		}
-	}
+	public var gfVersion:String = '';
+	public var noteStyle:String = '';
+	public var stage:String = '';
 
 	public function new(song, notes, bpm)
 	{
@@ -90,20 +46,18 @@ class Song
 
 	public static function loadFromJson(jsonInput:String, ?folder:String):SwagSong
 	{
-		var rawJson = null;
-		
-		var formattedFolder:String = Paths.formatToSongPath(folder);
-		var formattedSong:String = Paths.formatToSongPath(jsonInput);
-		#if MODS_ALLOWED
-		var moddyFile:String = Paths.modsJson(formattedFolder + '/' + formattedSong);
-		if(FileSystem.exists(moddyFile)) {
-			rawJson = File.getContent(moddyFile).trim();
-		}
-		#end
+		trace(jsonInput);
 
-		if(rawJson == null) {
-			rawJson = File.getContent(Main.getDataPath() + Paths.json(formattedFolder + '/' + formattedSong)).trim();
+		// pre lowercasing the folder name
+		var folderLowercase = StringTools.replace(folder, " ", "-").toLowerCase();
+		switch (folderLowercase) {
+			case 'dad-battle': folderLowercase = 'dadbattle';
+			case 'philly-nice': folderLowercase = 'philly';
 		}
+		
+		trace('loading ' + folderLowercase + '/' + jsonInput.toLowerCase());
+
+		var rawJson = Assets.getText(Paths.json(folderLowercase + '/' + jsonInput.toLowerCase())).trim();
 
 		while (!rawJson.endsWith("}"))
 		{
@@ -127,10 +81,7 @@ class Song
 				daSong = songData.song;
 				daBpm = songData.bpm; */
 
-		var songJson:SwagSong = parseJSONshit(rawJson);
-		if(jsonInput != 'events') StageData.loadDirectory(songJson);
-		onLoadJson(songJson);
-		return songJson;
+		return parseJSONshit(rawJson);
 	}
 
 	public static function parseJSONshit(rawJson:String):SwagSong
